141002853d94bb6435d38490ab007418823aa0eb
aqjune <aqjune@gmail.com>
2017-11-02 21:02:17 +0900

    Update CaptureTracking.cpp so 'p == load(glbv)' captures p

--- lib/Analysis/CaptureTracking.cpp
+++ lib/Analysis/CaptureTracking.cpp
@@ -337,10 +337,10 @@ void llvm::PointerMayBeCaptured(const Value *V, CaptureTracker *Tracker) {
       // Comparison against value stored in global variable. Given the pointer
       // does not escape, its value cannot be guessed and stored separately in a
       // global variable.
-      unsigned OtherIndex = (I->getOperand(0) == V) ? 1 : 0;
-      auto *LI = dyn_cast<LoadInst>(I->getOperand(OtherIndex));
-      if (LI && isa<GlobalVariable>(LI->getPointerOperand()))
-        break;
+      //unsigned OtherIndex = (I->getOperand(0) == V) ? 1 : 0;
+      //auto *LI = dyn_cast<LoadInst>(I->getOperand(OtherIndex));
+      //if (LI && isa<GlobalVariable>(LI->getPointerOperand()))
+      //  break;
       // Otherwise, be conservative. There are crazy ways to capture pointers
       // using comparisons.
       if (Tracker->captured(U))
