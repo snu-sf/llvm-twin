aqjune/llvm-intptr

Disable GVN for pointer comparison

lib/Transforms/Scalar/GVN.cpp
@@ -2045,8 +2045,9 @@ bool GVN::propagateEquality(Value *LHS, Value *RHS, const BasicBlockEdge &Root,
 
       // If "A == B" is known true, or "A != B" is known false, then replace
       // A with B everywhere in the scope.
-      if ((isKnownTrue && Cmp->getPredicate() == CmpInst::ICMP_EQ) ||
-          (isKnownFalse && Cmp->getPredicate() == CmpInst::ICMP_NE))
+      if (!Op->getType()->isPtrOrPtrVectorTy() &&
+          ((isKnownTrue && Cmp->getPredicate() == CmpInst::ICMP_EQ) ||
+          (isKnownFalse && Cmp->getPredicate() == CmpInst::ICMP_NE)))
         Worklist.push_back(std::make_pair(Op0, Op1));
 
       // Handle the floating point versions of equality comparisons too.

snu-sf/llvm-twin

lib/Transforms/Scalar/GVN.cpp
@@ -1809,8 +1809,9 @@ bool GVN::propagateEquality(Value *LHS, Value *RHS, const BasicBlockEdge &Root,
 
       // If "A == B" is known true, or "A != B" is known false, then replace
       // A with B everywhere in the scope.
-      if ((isKnownTrue && Cmp->getPredicate() == CmpInst::ICMP_EQ) ||
-          (isKnownFalse && Cmp->getPredicate() == CmpInst::ICMP_NE))
+      if (!Op0->getType()->isPtrOrPtrVectorTy() &&
+          ((isKnownTrue && Cmp->getPredicate() == CmpInst::ICMP_EQ) ||
+          (isKnownFalse && Cmp->getPredicate() == CmpInst::ICMP_NE)))
         Worklist.push_back(std::make_pair(Op0, Op1));
 
       // Handle the floating point versions of equality comparisons too.